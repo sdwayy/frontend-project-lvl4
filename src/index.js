7810e8e7600fe5386dc2d100729307a603c4974a
Daniil <set2901@mail.ru>
2020-11-27 22:35:44 +0300

    Добавляет базовую конфигурацию компонента channels

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-// @ts-check
-
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
 import '../assets/application.scss';
-
 // import faker from 'faker';
 import gon from 'gon';
 // import cookies from 'js-cookie';
 // import io from 'socket.io-client';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './modules/App.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
@@ -16,3 +15,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 console.log('it works!');
 console.log('gon', gon)
+
+render(
+  <App />,
+  document.querySelector('.container')
+);
