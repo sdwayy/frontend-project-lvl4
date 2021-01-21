10f3a198ea6a3b037277dfaf299b0cef314e1d74
Daniil <set2901@mail.ru>
2021-01-21 19:34:08 +0300

    исправляет ошибку линтера

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import '../assets/application.scss';
 import io from 'socket.io-client';
-import init from './init';
 import gon from 'gon';
+import init from './init';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
